658eabb493b7e3d88033196932fae2c755e81258
eniolafashola <fasholaeniola111@gmail.com>
2023-03-17 00:34:56 +0100

    ACtual first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Helloooo Jupiter</Text>
+      <Text>Blah blah blah sheep</Text>
       <StatusBar style="auto" />
     </View>
   );
